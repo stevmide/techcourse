b67b3f40701df382d1da5f89f10c97a5f67f7ee5
stevmide <olu.ojumoola@gmail.com>
2024-05-07 10:01:08 +0100

    new changes

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ const Home = () => {
   return (
     <>
       <Head>
-        <title>Nextly - Free Nextjs & TailwindCSS Landing Page Template</title>
+        <title>AI Pro</title>
         <meta
           name="description"
           content="Nextly is a free landing page template built with next.js & Tailwind CSS"
